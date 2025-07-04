be0cdf54a0109225e9be4a6837422d4ca8f03265
t13846 <t13846@ms.sltung.com.tw>
2025-07-04 10:28:04 +0800

    feat: add analytics tracking with vue-gtag

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,11 +2,17 @@ import { createApp } from 'vue'
 import { createPinia } from "pinia";
 import router from "./router";
 import './style.css'
+import { createGtag } from "vue-gtag";
 
 import App from './App.vue'
+
+const gtag = createGtag({
+  tagId: `${import.meta.env.VITE_ANALYTICS_ID}`
+})
 
 const app = createApp(App);
 const pinia = createPinia();
 app.use(pinia);
 app.use(router);
+app.use(gtag)
 app.mount('#app')
