908f08cbfc495d0a7a9111856c7258bb8fef4cb2
t13846 <t13846@ms.sltung.com.tw>
2025-07-04 11:01:47 +0800

    feat: add Google Tag Manager for analytics tracking

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,17 +2,22 @@ import { createApp } from 'vue'
 import { createPinia } from "pinia";
 import router from "./router";
 import './style.css'
-import { createGtag } from "vue-gtag";
+import { createGtm } from '@gtm-support/vue-gtm'
 
 import App from './App.vue'
-
-const gtag = createGtag({
-  tagId: `${import.meta.env.VITE_ANALYTICS_ID}`
-})
 
 const app = createApp(App);
 const pinia = createPinia();
 app.use(pinia);
 app.use(router);
-app.use(gtag)
+app.use(createGtm({
+  id: `${import.meta.env.VITE_GTM_ID}`,
+  defer: false,
+  compatibility: false,
+  enabled: true,
+  debug: true,
+  loadScript: true,
+  vueRouter: router,
+  trackOnNextTick: false,
+}))
 app.mount('#app')
