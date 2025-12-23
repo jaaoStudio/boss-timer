e3a010da36c04b58f5efdc8cafbc5c3cd44d7af3
t13846 <t13846@ms.sltung.com.tw>
2025-12-23 17:02:54 +0800

    refactor(ui): change google theme filled_blue to filled_black

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -68,7 +68,7 @@ const showGoogleButton = ref(true);
 const googleButtonConfig = computed(() => ({
   type: 'standard',
   size: 'medium',
-  theme: isDark.value ? 'filled_blue' : 'outline',
+  theme: isDark.value ? 'filled_black' : 'outline',
 }));
 
 watch(isDark, async () => {
